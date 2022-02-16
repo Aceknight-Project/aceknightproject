8c71c9633b1ccba134902c525ce329519bc04b25
PieRow <hoangnhatminh.139@gmail.com>
2022-02-16 16:06:26 +0700

    Add Base Movement
    Made the base for player movement

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    [SerializeField]
+    CharacterController2D controller2D;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    [SerializeField]
+    float runSpeed = 40f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,18 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+    }
+    void FixedUpdate()
+    {
+        controller2D.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
