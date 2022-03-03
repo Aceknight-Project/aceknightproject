3dd72bdbea7e84bb6cf35965f880c3ae72be15ce
Phạm Quang Thái <73540939+thai96@users.noreply.github.com>
2022-03-03 10:46:16 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/Aceknight-Project/aceknightproject""
    This reverts commit d4bf556e4ecddf5c4d247a5973fd71be64f43c45.

---
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    [SerializeField]
+    Animator legAnimator;
+    [SerializeField]
+    CharacterController2D controller2D;
+    protected float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    [SerializeField]
+    float runSpeed = 40f;
+    public float healthPoint;
+    float overhealTimer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPoint = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        legAnimator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            legAnimator.SetBool("IsJumping", true);
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+            crouch = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+        OverhealControl();
+    }
+    void FixedUpdate()
+    {
+        controller2D.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+    public float GetSpeed()
+    {
+        return horizontalMove;
+    }
+    public bool GetState()
+    {
+        return jump;
+    }
+    public float GetHealth()
+    {
+        return healthPoint;
+    }
+    public float TakeDamage(float damage)
+    {
+        healthPoint -= damage;
+        return healthPoint;
+    }
+    public float Heal(float heal)
+    {
+        healthPoint += heal;
+        if (healthPoint > 130f) healthPoint = 130f;
+        return healthPoint;
+    }
+    void OverhealControl()
+    {
+        if (healthPoint > 100)
+        {
+            overhealTimer += Time.deltaTime;
+            if (overhealTimer >= 1f)
+            {
+                healthPoint -= 1f;
+                overhealTimer = 0f;
+            }
+        }
+    }
+    public void OnLanding()
+    {
+        legAnimator.SetBool("IsJumping", false);
+    }
+}
