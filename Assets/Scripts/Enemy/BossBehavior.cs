db5ae86acdd4623fa03bead5623f931ce34fdcba
xuanzhoag <88784405+xuanzhoag@users.noreply.github.com>
2022-03-09 15:31:17 +0700

    BossUpdate_2.0
    update Boss and healthbar

--- Assets/Scripts/Enemy/BossBehavior.cs
+++ Assets/Scripts/Enemy/BossBehavior.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class BossBehavior : MonoBehaviour
 {
- 
+
     [SerializeField]
     GameObject bullet;
+
     float time = 0f;
-  
-    
+
+    public bool isInvulnerable = false;
+    public float healthPoint;
+    public float currentHealth;
+
+    public BossHealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
-      
-       
-        
+        healthPoint = 1000000f;
+        currentHealth = healthPoint;
+        healthBar.setMaxHealth(healthPoint);
     }
 
     // Update is called once per frame
@@ -27,9 +32,25 @@ public class BossBehavior : MonoBehaviour
             GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
             time = 0f;
         }
-      
+
+
 
     }
 
+    public void takeDamage(int damage)
+    {
+        if (isInvulnerable) return;
+
+        healthPoint -= damage;
+
+        healthBar.setHealth(healthPoint);
+
+        if (healthPoint < 0) Die();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
 
 }
